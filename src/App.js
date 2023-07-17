8d656111f19357904b7b6a2848d46a2d413e5d77
radwan <www.radwaniq@gmail.com>
2023-07-18 02:37:52 +0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("")
+
+  const handleRemove = ()=>{
+    setValue("")
+  }
+
+  const handleBack = ()=>{
+    setValue(value.slice(0,-1))
+  }
+
+  const evalueExpresion =()=>{
+    try{
+      const evaluar = eval(value)
+      setValue(evaluar.toString())
+    }catch(err){
+      console.log(err)
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="border">
+        <input type="text" className="display" value={value}  onClick={e => setValue(value + e.target.value)}/>
+          <button value="1" onClick={e => setValue(value + e.target.value)}>1</button>
+          <button value="2" onClick={e => setValue(value + e.target.value)}>2</button>
+          <button value="3" onClick={e => setValue(value + e.target.value)}>3</button>
+          <button value="/" onClick={e => setValue(value + e.target.value)}>/</button>
+          <button value="4" onClick={e => setValue(value + e.target.value)}>4</button>
+          <button value="5" onClick={e => setValue(value + e.target.value)}>5</button>
+          <button value="6" onClick={e => setValue(value + e.target.value)}>6</button>
+          <button value="*" onClick={e => setValue(value + e.target.value)}>*</button>
+          <button value="7" onClick={e => setValue(value + e.target.value)}>7</button>
+          <button value="8" onClick={e => setValue(value + e.target.value)}>8</button>
+          <button value="9" onClick={e => setValue(value + e.target.value)}>9</button>
+          <button value="+" onClick={e => setValue(value + e.target.value)}>+</button>
+          <button value="0" onClick={e => setValue(value + e.target.value)}>0</button>
+          <button value="." onClick={e => setValue(value + e.target.value)}>.</button>
+          <button value="RESET" onClick={handleRemove}>R</button>
+          <button value="Back" onClick={handleBack}>Back</button>
+          <button value="-" onClick={e => setValue(value + e.target.value)}>-</button>
+          <button value="=" onClick={evalueExpresion}>=</button>
+      </div>
     </div>
   );
 }
