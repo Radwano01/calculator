2b411ce7db28845b18896ed4f7de26b34e709f39
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-08-02 06:44:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,69 @@
-
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState('');
 
-  const handleRemove = ()=>{
-    setValue("")
-  }
+  const handleRemove = () => {
+    setValue('');
+  };
 
-  const handleBack = ()=>{
-    setValue(value.slice(0,-1))
-  }
+  const handleBack = () => {
+    setValue(value.slice(0, -1));
+  };
 
-  const evalueExpresion =()=>{
-    try{
-      const evaluar = eval(value)
-      setValue(evaluar.toString())
-    }catch(err){
-      console.log(err)
+  const evaluateExpression = () => {
+    try {
+      const result = eval(value);
+      setValue(result.toString());
+    } catch (err) {
+      console.log(err);
     }
-  }
-  
+  };
+
+  const handleButtonClick = (e) => {
+    setValue(value + e.target.value);
+  };
+
   return (
     <div className="calculator">
-      <div className="border">
-        <input type="text" className="display" value={value}  onClick={e => setValue(value + e.target.value)}/>
-          <button value="1" onClick={e => setValue(value + e.target.value)}>1</button>
-          <button value="2" onClick={e => setValue(value + e.target.value)}>2</button>
-          <button value="3" onClick={e => setValue(value + e.target.value)}>3</button>
-          <button value="/" onClick={e => setValue(value + e.target.value)}>/</button>
-          <button value="4" onClick={e => setValue(value + e.target.value)}>4</button>
-          <button value="5" onClick={e => setValue(value + e.target.value)}>5</button>
-          <button value="6" onClick={e => setValue(value + e.target.value)}>6</button>
-          <button value="*" onClick={e => setValue(value + e.target.value)}>*</button>
-          <button value="7" onClick={e => setValue(value + e.target.value)}>7</button>
-          <button value="8" onClick={e => setValue(value + e.target.value)}>8</button>
-          <button value="9" onClick={e => setValue(value + e.target.value)}>9</button>
-          <button value="+" onClick={e => setValue(value + e.target.value)}>+</button>
-          <button value="0" onClick={e => setValue(value + e.target.value)}>0</button>
-          <button value="." onClick={e => setValue(value + e.target.value)}>.</button>
-          <button value="RESET" onClick={handleRemove}>R</button>
-          <button value="Back" onClick={handleBack}>Back</button>
-          <button value="-" onClick={e => setValue(value + e.target.value)}>-</button>
-          <button value="=" onClick={evalueExpresion}>=</button>
+      <div className="display-container">
+        <input
+          type="text"
+          className="display"
+          value={value}
+          readOnly
+        />
+      </div>
+      <div className="buttons-container">
+        <div className="row">
+          <button className="numeric" value="7" onClick={handleButtonClick}>7</button>
+          <button className="numeric" value="8" onClick={handleButtonClick}>8</button>
+          <button className="numeric" value="9" onClick={handleButtonClick}>9</button>
+          <button className="operator" value="/" onClick={handleButtonClick}>/</button>
+        </div>
+        <div className="row">
+          <button className="numeric" value="4" onClick={handleButtonClick}>4</button>
+          <button className="numeric" value="5" onClick={handleButtonClick}>5</button>
+          <button className="numeric" value="6" onClick={handleButtonClick}>6</button>
+          <button className="operator" value="*" onClick={handleButtonClick}>*</button>
+        </div>
+        <div className="row">
+          <button className="numeric" value="1" onClick={handleButtonClick}>1</button>
+          <button className="numeric" value="2" onClick={handleButtonClick}>2</button>
+          <button className="numeric" value="3" onClick={handleButtonClick}>3</button>
+          <button className="operator" value="+" onClick={handleButtonClick}>+</button>
+        </div>
+        <div className="row">
+          <button className="numeric" value="0" onClick={handleButtonClick}>0</button>
+          <button className="numeric" value="." onClick={handleButtonClick}>.</button>
+          <button className="special" value="RESET" onClick={handleRemove}>R</button>
+          <button className="operator" value="-" onClick={handleButtonClick}>-</button>
+        </div>
+        <div className="row">
+          <button className="equal" value="=" onClick={evaluateExpression}>=</button>
+          <button className="special" value="Back" onClick={handleBack}>Back</button>
+        </div>
       </div>
     </div>
   );
